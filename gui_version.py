15e0b8fbbd632f91bf1410620cf3f7a4996cd96b
Annabella Gethke <annabella.gethke@hu-berlin.de>
2023-12-01 21:10:58 +0100

    http lol

--- gui_version.py
+++ gui_version.py
@@ -45,7 +45,7 @@ def upload_dpsreport(file_to_upload, domain):
     elif domain == "b":
         url = "https://b.dps.report/uploadContent"
     elif domain == "c":
-        url = "https://a.dps.report/uploadContent"
+        url = "http://a.dps.report/uploadContent"
     files = {'file': (file_to_upload, open(file_to_upload, 'rb'))}
     data = {'json': '1', 'generator': 'ei'}
 
@@ -110,7 +110,7 @@ while True:
                 file_path = os.path.join(root, file)
                 file_mtime = os.path.getmtime(file_path)
                 # Select only files that we havent seen before and that are created after starttime of the program
-                if file_path not in seen_files and file_mtime > start_time:
+                if file_path not in seen_files and file_mtime != start_time:
                     print(get_current_time(),"New file detected:",file)
                     seen_files.add(file_path)
                     success_value, dps_link = upload_dpsreport(file_path,"a")
