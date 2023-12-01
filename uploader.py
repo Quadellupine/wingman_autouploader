7b559e90c4ae0b25acdf969c97972a486b0f46b0
Annabella Gethke <annabella.gethke@hu-berlin.de>
2023-12-01 18:23:58 +0100

    regular uploader updates

--- uploader.py
+++ uploader.py
@@ -23,26 +23,36 @@ def upload_wingman(dps_link):
     print(get_current_time(),data['note'])
 
 
-def upload_dpsreport(file_to_upload):
-    url = "https://dps.report/uploadContent"
+def upload_dpsreport(file_to_upload, domain):
+    if domain == "a":
+        url = "https://dps.report/uploadContent"
+    elif domain == "b":
+        url = "https://b.dps.report/uploadContent"
+    elif domain == "c":
+        url = "https://a.dps.report/uploadContent"
     files = {'file': (file_to_upload, open(file_to_upload, 'rb'))}
     data = {'json': '1', 'generator': 'ei'}
 
     response = requests.post(url, files=files, data=data)
-    if response.status_code != 200:
-        print(get_current_time(),"An error has occured while uploadng to dps.report, aborting...")
+
+    if response.status_code != 200:        
+        success_value = False
+        dps_link = "skip"
+        print(get_current_time(),"An error has occured while uploadng to dps.report")
         print(get_current_time(),"Errorcode:",response.status_code)
-        if response.status_code == 403:
-            print(get_current_time(),"Most likely ratelimited: Retrying in 30 seconds.")
-            os.sleep(30)
-            # this could be a recursive hellscape
-            upload_dpsreport(file_to_upload)
-        return False
+        if response.status_code == 403 and domain=="a":
+            print(get_current_time(),"Trying b.dps.report")
+            success_value,dps_link = upload_dpsreport(file_to_upload, "b")
+        elif response.status_code == 403 and domain =="b":
+            print(get_current_time(), "Trying a.dps.report")
+            success_value,dps_link = upload_dpsreport(file_to_upload, "c")
+        return success_value, dps_link
 
     data = response.json()
     print(get_current_time(),"permalink:", data['permalink'])
     dps_link = data['permalink']
-    return dps_link
+    success_value = data.get('encounter', {}).get('success')
+    return success_value, dps_link
 
 
 seen_files = set()
@@ -58,10 +68,11 @@ while True:
             if file.endswith(".zevtc"):
                 file_path = os.path.join(root, file)
                 file_mtime = os.path.getmtime(file_path)
-                if file_path not in seen_files and file_mtime > start_time:
+                if file_path not in seen_files and file_mtime != start_time:
                     print(get_current_time(),"New file detected:",file)
                     seen_files.add(file_path)
-                    dps_link = upload_dpsreport(file_path) 
-                    upload_wingman(dps_link)
+                    success_value, dps_link = upload_dpsreport(file_path, "a") 
+                    if success_value:
+                        upload_wingman(dps_link)
 
     time.sleep(polling_interval)
